2c7b5d1838d4206c5f5bea5164a8c375911d6f9e
M V <vahyno+learn@gmail.com>
2017-09-12 16:11:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = new Object();
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object,{[key]:value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+
+}
